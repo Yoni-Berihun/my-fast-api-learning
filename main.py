ebb82c6d15e2fcc1bf2e145bcc0860ecb939bc03
Yoni-Berihun <yonatanberihun1998@gmail.com>
2025-12-15 21:51:11 +0300

    Add Product model and user addition endpoint to FastAPI application

--- main.py
+++ main.py
@@ -8,9 +8,20 @@ class Profile(BaseModel):
     age:int
 
 
+class Product(BaseModel):
+    name:str
+    price:int
+    discount:int
+    discounted_price:int
+    
 #creating instance of the class fast API
 app = FastAPI()
 
 @app.post('/adduser')
 def adduser(profile:Profile):
     return profile
+#using model inside a function
+@app.post('/addproduct')
+def addproduct(product:Product):
+    product.discounted_price = product.price - (product.price * product.discount)/100
+    return product
