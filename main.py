b65bb058d8b37f234193d440a635737b46a80ab8
Yoni-Berihun <yonatanberihun1998@gmail.com>
2025-12-15 21:13:16 +0300

    Add Profile model and user addition endpoint to FastAPI application

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 #importing the module 
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Profile(BaseModel):
+    name:str
+    email:str
+    age:int
+
 
 #creating instance of the class fast API
 app = FastAPI()
+
+@app.post('/adduser')
+def adduser(profile:Profile):
+    return profile
