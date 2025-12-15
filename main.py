ff4938f99dbbba70be554aef77c41607e601c74c
Yoni-Berihun <yonatanberihun1998@gmail.com>
2025-12-15 22:40:17 +0300

    feat: Add User model and `/purchase` endpoint, and enhance `/addproduct` to accept path parameters.

--- main.py
+++ main.py
@@ -14,14 +14,22 @@ class Product(BaseModel):
     discount:int
     discounted_price:int
     
+class User(BaseModel):
+    name:str
+    email:str
+       
 #creating instance of the class fast API
 app = FastAPI()
+
+@app.post('/purchase')
+def purchase(user:User,product:Product):
+    return {"user": user, "product":product}
 
 @app.post('/adduser')
 def adduser(profile:Profile):
     return profile
 #using model inside a function
-@app.post('/addproduct')
-def addproduct(product:Product):
+@app.post('/addproduct/{product_id}')
+def addproduct(product:Product, product_id:int, category:str):
     product.discounted_price = product.price - (product.price * product.discount)/100
-    return product
+    return {"Product_Id":product_id, "Product":product, 'category':category}
