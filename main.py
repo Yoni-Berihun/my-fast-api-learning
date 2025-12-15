b57f7aa706483a0d573dcb6210bf500c29e52966
Yoni-Berihun <yonatanberihun1998@gmail.com>
2025-12-15 20:34:37 +0300

    Add query parameters for products and items, and implement about route with path and query parameters

--- main.py
+++ main.py
@@ -47,5 +47,19 @@ def page(name):
 
 #query parametres
 @app.get('/products')
-def products(id,price):
-    return {f'product with and id: {id},{price}'}
+def products(id =1,price=0):
+    return {f'product with and id: {id} and price :{price}'}
+
+#we can also give specific data type and also
+# provide default values for the query parameter
+@app.get('/items')
+def items(id:int=1, qty:int=0):
+    return{f'item with an id:{id} and with quuantity of:{qty}'}
+
+#using path and query parametres simoultaneously
+@app.get('/about/{userid}/comments')
+def about(userid:int,commentid:int):
+    return{f'About page for the user with user id: {userid} and comment with: {commentid}'}
+#this is how we use to add it on browses http://127.0.0.1:8000/about/224/comments?commentid=98
+
+#required query parametres
